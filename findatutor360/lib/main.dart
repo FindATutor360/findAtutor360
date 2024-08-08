b83ad71000fc5cd9517400e678969c0084fa315e
asamoah-fred <asamoahgodfred42@gmail.com>
2024-08-08 22:01:31 +0000

    Initialized firebase in app

--- findatutor360/lib/main.dart
+++ findatutor360/lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:findatutor360/firebase_options.dart';
 import 'package:findatutor360/theme/index.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'routes/index.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
