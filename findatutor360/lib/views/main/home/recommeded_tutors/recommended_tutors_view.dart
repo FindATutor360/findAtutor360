af66d35015e4e924d879442d7c991e0ed640b7db
asamoah-fred <asamoahgodfred42@gmail.com>
2024-08-19 13:12:51 +0000

    Recommended UI

--- findatutor360/lib/views/main/home/recommeded_tutors/recommended_tutors_view.dart
+++ findatutor360/lib/views/main/home/recommeded_tutors/recommended_tutors_view.dart
@@ -1,13 +1,42 @@
+import 'package:findatutor360/custom_widgets/card/recommended_tutor_card.dart';
+import 'package:findatutor360/custom_widgets/header/back_icon_header.dart';
+import 'package:findatutor360/theme/index.dart';
 import 'package:flutter/material.dart';
 
 class RecommendedTutorsView extends StatelessWidget {
   const RecommendedTutorsView({super.key});
-  static const path = '/recommendedTutors';
+  static const path = 'recommendedTutors';
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Recommended Tutors')),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: customTheme['whiteColor'],
+      appBar: const BackIconHeader(
+        header: 'Recommended Tutors',
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 24,
+          ),
+          Expanded(
+            child: GridView.builder(
+              shrinkWrap: true,
+              itemCount: 10,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.7,
+                  mainAxisSpacing: 15,
+                  crossAxisSpacing: 15),
+              itemBuilder: ((BuildContext context, int index) {
+                return const RecommededTutorCard();
+              }),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
